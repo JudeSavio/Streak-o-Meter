bf73a3ff138557eac3a17985adba2db7dd6f897c
Jude_Savio <judealphonsesavio@gmail.com>
2021-07-13 00:30:08 +0530

    Restructured the card composition and flex box settings with width & height

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ function plus(){
     number = parseInt(number) + 1;
     number = Number(number)
     if(number >= 100 && number<=999){
-        document.getElementsByClassName("num")[0].style= "font-size:900%";
         document.getElementsByClassName("num")[0].innerHTML = number
     }
     else if (number>=1000 && number<=9999){
-        document.getElementsByClassName("num")[0].style= 
-        "font-size:800%";
         document.getElementsByClassName("num")[0].innerHTML = number
     }
     else if(number >= 10000 && number <= 99999){
-        document.getElementsByClassName("num")[0].style= 
-        "font-size:600%; ";
-        document.getElementsByClassName("plus")[0].style= 
-        "margin-top:35px; width:50px; height:50px ";
         document.getElementsByClassName("num")[0].innerHTML = number
     }
     else{
@@ -35,12 +28,9 @@ function minus(){
         document.getElementsByName("minus:active").style.transform = "translateY(0px)";
     }
     else if(number >= 100 && number<=999){
-        document.getElementsByClassName("num")[0].style= "font-size:900%";
         document.getElementsByClassName("num")[0].innerHTML = number
     }
     else if(number >= 1000 && number<=9999){
-        document.getElementsByClassName("num")[0].style= 
-        "font-size:800%";
         document.getElementsByClassName("num")[0].innerHTML = number
     }
     else{
@@ -57,6 +47,8 @@ function color_picker(className){
         "background-image: linear-gradient(to right,#e92efb,#ff2079,#440bd4,#04005e);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
         document.getElementsByClassName("banner")[0].style=
         "background-image: linear-gradient(to right,#e92efb,#ff2079,#440bd4,#04005e);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
+        document.getElementsByClassName("num")[0].style=
+        "background-image: linear-gradient(to right,#e92efb,#ff2079,#440bd4,#04005e);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
     }
     else if(className=='color_pallete_two'){
         console.log('two working')
@@ -64,11 +56,15 @@ function color_picker(className){
         "background-image: linear-gradient(to right,#ddd429,#63c007,#fb33db,#0310ea);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
         document.getElementsByClassName("banner")[0].style=
         "background-image: linear-gradient(to right,#ddd429,#63c007,#fb33db,#0310ea);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
+        document.getElementsByClassName("num")[0].style=
+        "background-image: linear-gradient(to right,#ddd429,#63c007,#fb33db,#0310ea);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
     }
     else if(className=='color_pallete_three'){
         document.getElementsByClassName("streak-container")[0].style=
         "background-image: linear-gradient(to right,#b9b605,#0496f7,#ff822e,#f21a1d);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
         document.getElementsByClassName("banner")[0].style=
+        "background-image: linear-gradient(to right,#b9b605,#0496f7,#ff822e,#f21a1d);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
+        document.getElementsByClassName("num")[0].style=
         "background-image: linear-gradient(to right,#b9b605,#0496f7,#ff822e,#f21a1d);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
     }
     else if (className=='color_pallete_four'){
@@ -76,11 +72,15 @@ function color_picker(className){
         "background-image: linear-gradient(to right,#e300fc,#ce0f65,#ff5f01,#ce0000);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
         document.getElementsByClassName("banner")[0].style=
         "background-image: linear-gradient(to right,#e300fc,#ce0f65,#ff5f01,#ce0000);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
+        document.getElementsByClassName("num")[0].style=
+        "background-image: linear-gradient(to right,#e300fc,#ce0f65,#ff5f01,#ce0000);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
     }
     else if (className=='color_pallete_five'){
         document.getElementsByClassName("streak-container")[0].style=
         "background-image: linear-gradient(to right,#bdbdfd,#bc7579,#535eeb,#00218a);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
         document.getElementsByClassName("banner")[0].style=
+        "background-image: linear-gradient(to right,#bdbdfd,#bc7579,#535eeb,#00218a);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
+        document.getElementsByClassName("num")[0].style=
         "background-image: linear-gradient(to right,#bdbdfd,#bc7579,#535eeb,#00218a);background-size: 300%;background-image: -100%;animation : backgroundShift 5s infinite alternate-reverse";
     }
 }
